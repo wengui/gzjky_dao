7fa0b1634e754aa7826bf31b7f6378c52a6d1983
yuting0787 <626808708@qq.com>
2016-01-11 14:19:45 +0800

    做成

--- src/main/java/com/gzjky/dao/readdao/BloodPressureHistoryReadMapper.java
+++ src/main/java/com/gzjky/dao/readdao/BloodPressureHistoryReadMapper.java
@@ -1,8 +1,15 @@
 package com.gzjky.dao.readdao;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.gzjky.bean.extend.QueryBloodPressureOutputBean;
 import com.gzjky.bean.gen.BloodPressureHistory;
 
 public interface BloodPressureHistoryReadMapper {
 
     BloodPressureHistory selectByPrimaryKey(Integer id);
+    
+    List<QueryBloodPressureOutputBean> selectBloodPressureByCondition(@Param("TestingTime")String TestingTime, @Param("State")String State);
 }
