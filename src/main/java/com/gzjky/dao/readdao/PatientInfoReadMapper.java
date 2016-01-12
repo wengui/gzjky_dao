2e7d4a0fbfe4b99b8856c5da4a34c10d7884db9d
yuting0787 <626808708@qq.com>
2016-01-12 18:24:51 +0800

    做成

--- src/main/java/com/gzjky/dao/readdao/PatientInfoReadMapper.java
+++ src/main/java/com/gzjky/dao/readdao/PatientInfoReadMapper.java
@@ -1,8 +1,18 @@
 package com.gzjky.dao.readdao;
 
+import org.apache.ibatis.annotations.Param;
+
+import com.gzjky.bean.extend.PatientInfoOutputBean;
 import com.gzjky.bean.gen.PatientInfo;
 
 public interface PatientInfoReadMapper {
 
     PatientInfo selectByPrimaryKey(Integer id);
+    
+    /**
+     * 查询患者基本信息
+     * @param id
+     * @return
+     */
+    PatientInfoOutputBean selectByPatientId(@Param("id")Integer id);
 }
