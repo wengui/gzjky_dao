4b00c2e9d156098593000678cba1647d70ed1c48
zmj <zmj@zmj-PC>
2016-01-15 14:25:06 +0800

    2016/1/15 登录时session中加入patient信息

--- src/main/java/com/gzjky/dao/readdao/UserAndPatientReadMapper.java
+++ src/main/java/com/gzjky/dao/readdao/UserAndPatientReadMapper.java
@@ -1,8 +1,10 @@
 package com.gzjky.dao.readdao;
 
 import com.gzjky.bean.gen.UserAndPatient;
-
+import java.util.List;
 public interface UserAndPatientReadMapper {
 
     UserAndPatient selectByPrimaryKey(Integer id);
+       
+    List<UserAndPatient> selectByUserId(Integer id);
 }
