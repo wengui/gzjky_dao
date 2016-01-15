f941a59b0ab9b71f44174dbfded36126db8d3073
yuting0787 <626808708@qq.com>
2016-01-15 11:09:17 +0800

    做成

---
+++ src/main/java/com/gzjky/dao/readdao/NowComplicationsConfigReadMapper.java
@@ -0,0 +1,9 @@
+package com.gzjky.dao.readdao;
+
+import com.gzjky.bean.gen.NowComplicationsConfig;
+
+public interface NowComplicationsConfigReadMapper {
+
+    NowComplicationsConfig selectByPrimaryKey(Integer id);
+
+}
--- src/main/java/com/gzjky/dao/readdao/PatientNowComplicationsReadMapper.java
+++ src/main/java/com/gzjky/dao/readdao/PatientNowComplicationsReadMapper.java
@@ -1,8 +1,12 @@
 package com.gzjky.dao.readdao;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.gzjky.bean.gen.PatientNowComplications;
 
 public interface PatientNowComplicationsReadMapper {
 	
     PatientNowComplications selectByPrimaryKey(Integer id);
+    
+    PatientNowComplications selectByPatientId(@Param("patientId")Integer patientId);
 }
