164ff49e1fb1b8a33a86c4d8ae92e0e0a4774a2e
yuting0787 <626808708@qq.com>
2016-01-17 09:43:58 +0800

    做成

--- src/main/java/com/gzjky/dao/readdao/PatientHealthCheckReadMapper.java
+++ src/main/java/com/gzjky/dao/readdao/PatientHealthCheckReadMapper.java
@@ -1,8 +1,14 @@
 package com.gzjky.dao.readdao;
 
+import java.util.List;
+
+import com.gzjky.bean.extend.PatientHealthCheckInputBean;
+import com.gzjky.bean.extend.PatientHealthCheckOutputBean;
 import com.gzjky.bean.gen.PatientHealthCheck;
 
 public interface PatientHealthCheckReadMapper {
 
     PatientHealthCheck selectByPrimaryKey(Integer id);
+    
+    List<PatientHealthCheckOutputBean> selectByPatientId(PatientHealthCheckInputBean record);
 }
