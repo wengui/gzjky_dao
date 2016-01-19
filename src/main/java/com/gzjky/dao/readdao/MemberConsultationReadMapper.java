942b109169b43c0e42e63ac9eda448f1b459c7b4
yuting0787 <yuting0787@163.com>
2016-01-19 16:33:50 +0800

    最新咨询

--- src/main/java/com/gzjky/dao/readdao/MemberConsultationReadMapper.java
+++ src/main/java/com/gzjky/dao/readdao/MemberConsultationReadMapper.java
@@ -1,8 +1,10 @@
 package com.gzjky.dao.readdao;
 
 import com.gzjky.bean.gen.MemberConsultation;
+import com.gzjky.bean.extend.QueryMemberConsultationOutPutBean;
 
 public interface MemberConsultationReadMapper {
 
     MemberConsultation selectByPrimaryKey(Integer id);
+    QueryMemberConsultationOutPutBean selectByPatientID(Integer patientId);
 }
