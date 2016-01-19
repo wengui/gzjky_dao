904e423d97038a3d569d43bd1ffb4956d81c1b93
yuting0787 <626808708@qq.com>
2016-01-19 12:44:02 +0800

    做成

--- src/main/java/com/gzjky/dao/readdao/FamilyPhoneReadMapper.java
+++ src/main/java/com/gzjky/dao/readdao/FamilyPhoneReadMapper.java
@@ -2,6 +2,8 @@ package com.gzjky.dao.readdao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.gzjky.bean.extend.FamilyPhoneOutputBean;
 import com.gzjky.bean.gen.FamilyPhone;
 
@@ -15,5 +17,13 @@ public interface FamilyPhoneReadMapper {
      * @return
      */
     List<FamilyPhoneOutputBean> selectByPatientId(Integer patientId);
+    
+    /**
+     * 
+     * @param patientId
+     * @param cellphone
+     * @return
+     */
+    int selectByPatientIdAndPhone(@Param("patientId")Integer patientId, @Param("cellphone")String cellphone);
 
 }
