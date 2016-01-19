9fd82ae8c29cfe285e89b4bfa6304f97ad4947da
yuting0787 <yuting0787@163.com>
2016-01-19 15:26:15 +0800

    主页健康管理

--- src/main/java/com/gzjky/dao/readdao/DoctorAdviceReadMapper.java
+++ src/main/java/com/gzjky/dao/readdao/DoctorAdviceReadMapper.java
@@ -1,8 +1,12 @@
 package com.gzjky.dao.readdao;
 
 import com.gzjky.bean.gen.DoctorAdvice;
+import com.gzjky.bean.extend.QueryDoctorAdviceOutputBean;
+import java.util.List;
 
 public interface DoctorAdviceReadMapper {
 
     DoctorAdvice selectByPrimaryKey(Integer id);
+    
+    List<QueryDoctorAdviceOutputBean> selectByPatientID(Integer id);
 }
