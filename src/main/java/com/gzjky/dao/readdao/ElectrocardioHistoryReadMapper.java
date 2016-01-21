8ea9785739d5c603de9bd15c1a720120bfb907c9
yuting0787 <626808708@qq.com>
2016-01-21 15:43:18 +0800

    心电详细做成

--- src/main/java/com/gzjky/dao/readdao/ElectrocardioHistoryReadMapper.java
+++ src/main/java/com/gzjky/dao/readdao/ElectrocardioHistoryReadMapper.java
@@ -2,8 +2,11 @@ package com.gzjky.dao.readdao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.gzjky.bean.extend.ElectrocardioInputBean;
 import com.gzjky.bean.extend.ElectrocardioOutputBean;
+import com.gzjky.bean.extend.QueryEcgRecordDetailOutputBean;
 import com.gzjky.bean.gen.ElectrocardioHistory;
 
 public interface ElectrocardioHistoryReadMapper {
@@ -16,4 +19,12 @@ public interface ElectrocardioHistoryReadMapper {
      * @return
      */
     List<ElectrocardioOutputBean> selectElectrocardioByCondition(ElectrocardioInputBean record);
+    
+    /**
+     * 心电图详细信息
+     * @param id
+     * @param patientId
+     * @return
+     */
+    QueryEcgRecordDetailOutputBean selectElectrocardioDetail(@Param("id")Integer id, @Param("patientId")Integer patientId);
 }
