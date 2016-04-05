27c308b4a8e36ef8ffab7ea0992b216d9532364b
tom <tom@tom-PC>
2016-04-05 15:27:31 +0800

    血压图做成

--- src/main/java/com/gzjky/dao/readdao/BloodPressureHistoryReadMapper.java
+++ src/main/java/com/gzjky/dao/readdao/BloodPressureHistoryReadMapper.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.ibatis.annotations.Param;
 
 import com.gzjky.bean.extend.QueryBPandHROutputBean;
+import com.gzjky.bean.extend.QueryBloodPressureApiOutputBean;
 import com.gzjky.bean.extend.QueryBloodPressureInputBean;
 import com.gzjky.bean.extend.QueryBloodPressureOutputBean;
 import com.gzjky.bean.gen.BloodPressureHistory;
@@ -18,5 +19,13 @@ public interface BloodPressureHistoryReadMapper {
     QueryBPandHROutputBean selectBPandHRresult(Integer id);
     
     List<QueryBloodPressureOutputBean> selectBloodPressureGraft(@Param("id")Integer id, @Param("bpType")String bpType);
+    
+    /**
+     * Api 血压折线图数据取得
+     * @param id
+     * @param bpType
+     * @return
+     */
+    List<QueryBloodPressureApiOutputBean> selectBloodApiPressureGraft(@Param("id")Integer id, @Param("bpType")String bpType);
 
 }
